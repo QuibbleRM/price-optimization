c74a80f07f5c5db9cf12545355f39139900ba618
Sidney Guuaro <guarosidney@gmail.com>
2023-10-23 14:31:13 +0800

    update module reference

--- setup.py
+++ setup.py
@@ -21,10 +21,6 @@ setup(
         packages=find_packages(),
         install_requires=REQUIRED_PACKAGES,
         keywords=['python', 'price optimization'],
-        package_data={
-            '': ['*.csv'],
-            'files': ['*.csv'],
-        },
         classifiers= [
             "Development Status :: 3 - Alpha",
             "Intended Audience :: Education",
